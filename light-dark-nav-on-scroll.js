4025c1d9f40c507d490b642fd65809b320344671
Kerbz_78 <46557368+Kristian-DH@users.noreply.github.com>
2024-01-01 22:13:30 +0100

    Update and rename change-nav-on-scroll.js to light-dark-nav-on-scroll.js

---
+++ light-dark-nav-on-scroll.js
@@ -0,0 +1,35 @@
+// Navbar change color from light to dark
+$(".is-section-light").each(function (index) {
+  ScrollTrigger.create({
+    trigger: $(this),
+    start: "top 10%",
+    end: "bottom 10%",
+    onEnter: () => {
+      $(
+        ".nav-blur, .logo-epivity-blur-nav, .nav-link-blur-nav, .paragraph-btn-blur-nav, .btn-nav-small-blur, .menu-icon-blur_line-top, .menu-icon-blur_line-middle, .menu-icon-blur_line-middle-inner, .menu-icon-blur_line-bottom, .btn-contact-blur-nav"
+      ).addClass("is-light");
+    },
+    onEnterBack: () => {
+      $(
+        ".nav-blur, .logo-epivity-blur-nav, .nav-link-blur-nav, .paragraph-btn-blur-nav, .btn-nav-small-blur, .menu-icon-blur_line-top, .menu-icon-blur_line-middle, .menu-icon-blur_line-middle-inner, .menu-icon-blur_line-bottom, .btn-contact-blur-nav"
+      ).addClass("is-light");
+    },
+  });
+});
+$(".is-section-dark").each(function (index) {
+  ScrollTrigger.create({
+    trigger: $(this),
+    start: "top 10%",
+    end: "bottom 10%",
+    onEnter: () => {
+      $(
+        ".nav-blur, .logo-epivity-blur-nav, .nav-link-blur-nav, .paragraph-btn-blur-nav, .btn-nav-small-blur, .menu-icon-blur_line-top, .menu-icon-blur_line-middle, .menu-icon-blur_line-middle-inner, .menu-icon-blur_line-bottom, .btn-contact-blur-nav"
+      ).removeClass("is-light");
+    },
+    onEnterBack: () => {
+      $(
+        ".nav-blur, .logo-epivity-blur-nav, .nav-link-blur-nav, .paragraph-btn-blur-nav, .btn-nav-small-blur, .menu-icon-blur_line-top, .menu-icon-blur_line-middle, .menu-icon-blur_line-middle-inner, .menu-icon-blur_line-bottom, .btn-contact-blur-nav"
+      ).removeClass("is-light");
+    },
+  });
+});
